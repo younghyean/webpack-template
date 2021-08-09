8eec86f9751d1e8fb673fb7eb4a44e9ab193e326
yonghyen <ankim2son@gmail.com>
2021-08-09 21:38:39 +0900

    webpack

---
+++ .postcssrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Plugins: [
+    require('autoprefixer')
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+//import
+const path = require('path');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+  //파일을 읽어들이기 시작하는 진입점 설정 
+  entry: './js/main.js',
+  
+  //결과물(번들)을 반환하는 설정
+  output : {
+    // path : path.resolve(__dirname, 'dist'),
+    // filename : 'main.js',
+    clean : true
+  },
+
+  module : {
+    rules:[
+      {
+        test :/\.s?css$/,
+        use : [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+
+  //번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  // Plugins : [
+  //   new HtmlWebpackPlugin({ template: './index.html' }),
+  //   new CopyPlugin(
+  //     {
+  //      patterns:[
+  //         {from : 'static'},
+  //       ]
+  //     }
+  //   ),
+  // ],
+  
+  devServer: {
+    host : 'localhost',
+  }
+
+};
